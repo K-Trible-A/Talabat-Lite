d9dd357f94f4710ece69d311faa2f98e8f8d5984
AhmedHamdyMostafa <114616508+AhmedHamdyMostafa@users.noreply.github.com>
2024-11-10 02:35:44 +0200

    Customer.cpp file
    here I will implement functions that I have declared in header file and test it using database

---
+++ server/src/Customer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "sqlite/sqlite3.h"
+#include "Customer_h.h"
+// implement Customer functions that is decleared in customer header files
+// use sqlite3.h functions and quries
+using namespace std;
+int main()
+{
+    // code to test fuctions
+    // code to test database
+    return 0;
+}
