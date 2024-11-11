7aeb34956305e91e012377a5e0b212b35038a7b9
AbdoAli003 <abdoali0042@gmail.com>
2024-11-11 18:24:56 +0200

    Added card header file that represent the credit card information for clients

---
+++ server/include/card.hpp
@@ -0,0 +1,15 @@
+#define CNUM_SIZE 17
+#define EXP_SIZE 31  // maximum size for expiry date length
+class card 
+{
+private:
+
+    /* CREATE TABLE card (client_id INTEGER PRIMARY KEY , card_number TEXT NOT NULL , 
+            CVV INTEGER NOT NULL , expiry_date TEXT NOT NULL); */
+    public:
+        int clientId;
+        char cardNumber[CNUM_SIZE];
+        char expiryDate[EXP_SIZE];
+        int CVV;
+
+};
