257dd6af516dbd2fa21f050a59dc4def0420443f
KareemHatem <kareem.hatem013@gmail.com>
2024-11-11 21:35:04 +0200

    add constants header file and updated card header file

---
+++ server/include/constants.hpp
@@ -0,0 +1,9 @@
+const int MAX_NAME_LENGTH = 31;
+const int MAX_EMAIL_LENGTH = 61;
+const int MAX_ADDRESS_LENGTH = 101;
+const int MAX_PASSWORD_LENGTH = 31;
+const int NATIONAL_ID_LENGTH = 17;
+const int CARD_NUMBER_LENGTH = 17;
+const int CVV_LENGTH = 4;
+const int EXPIRE_DATE_LENGTH = 6;
+const int PHONE_NUMBER_LENGTH = 12;
