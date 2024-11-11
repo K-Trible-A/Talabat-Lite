c50251bf61633ce0f783c5c0814beac59df8fdcb
AbdoAli003 <abdoali0042@gmail.com>
2024-11-11 18:23:52 +0200

    Modified merchant header file : changed to camel case notation and added new variables

--- server/include/merchant.hpp
+++ server/include/merchant.hpp
@@ -1,24 +1,25 @@
 #include <sqlite3.h>
+#include "card.hpp"
 
-using namespace std;
-
-#define MAX_SIZE 30 // Maximum size for merhcant name, password, card number and national ID
-#define MAX_ADDRESS 100 // Maximum size for merhcant pickup address, email and keyword
+#define MAX_SIZE 31 // Maximum size for merhcant name, password, card number, phone number and national ID
+#define MAX_ADDRESS 101 // Maximum size for merhcant pickup address, email and keyword
                         
-enum {GROCERY = 1 , RESTAURANT = 2}; 
+enum {GROCERY = 1 , RESTAURANT = 2 , PHARMACY = 3}; 
 
 struct merch
 {
    int id;
-   int business_type;
+   int businessType;
    char name[MAX_SIZE];
-   char pickup_address[MAX_ADDRESS];
-   char card_num[MAX_SIZE];
+   char pickupAddress[MAX_ADDRESS];
    char email[MAX_ADDRESS];
    char password[MAX_SIZE];
-   char national_id[MAX_SIZE];
+   char nationalId[MAX_SIZE];
    char keywords[MAX_ADDRESS];
-   
+   char phone[MAX_SIZE];
+   char country[MAX_SIZE];
+   char city[MAX_SIZE];
+   card merchCard;
 
 };
  
@@ -26,9 +27,10 @@ class Merchant
 {
     private:
         /* 
-            CREATE TABLE merchant (id INTEGER PRIMARY KEY , name TEXT NOT NULL , pickup_address TEXT NOT NULL , 
-            card_number TEXT NOT NULL , email TEXT NOT NULL UNIQUE , password TEXT NOT NULL ,
-            national_id TEXT NOT NULL , keywords TEXT NOT NULL , business_type INTEGER NOT NULL);
+            CREATE TABLE merchant (id INTEGER PRIMARY KEY , name TEXT NOT NULL , pickup_address TEXT NOT NULL 
+            , email TEXT NOT NULL UNIQUE , password TEXT NOT NULL ,
+            national_id TEXT NOT NULL , keywords TEXT NOT NULL , phone TEXT NOT NULL ,business_type INTEGER NOT NULL ,
+            country TEXT NOT NULL , city TEXT NOT NULL);
          */ 
 
 
