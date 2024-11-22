6e0e473f34edd11994ecb93ae9be2492838ccfca
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2024-11-22 16:16:53 +0200

    Remove unused import

--- client/app/src/main/java/com/kaaa/talabat_lite/MainActivity.java
+++ client/app/src/main/java/com/kaaa/talabat_lite/MainActivity.java
@@ -2,7 +2,6 @@ package com.kaaa.talabat_lite;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
