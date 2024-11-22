1af301dea88ddd42a9f2728b142a4ed3beca8ae5
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2024-11-22 14:14:43 +0200

    Editing constants file
    Adding codes that will be indexes to and array of pointers to functions.
    To make each one of the team selects codes without overlap we chose
    prefix bias for each one.

--- server/include/constants.hpp
+++ server/include/constants.hpp
@@ -1,10 +1,16 @@
 #pragma once
-const int MAX_NAME_LENGTH = 31;
-const int MAX_EMAIL_LENGTH = 61;
-const int MAX_ADDRESS_LENGTH = 101;
-const int MAX_PASSWORD_LENGTH = 31;
-const int NATIONAL_ID_LENGTH = 17;
-const int CARD_NUMBER_LENGTH = 17;
-const int CVV_LENGTH = 4;
-const int EXPIRE_DATE_LENGTH = 6;
-const int PHONE_NUMBER_LENGTH = 12;
+
+typedef void (*funcPtr)(int);
+
+const int MAX_ACTIONS = 1e6;
+
+// Codes (Like a simple protocol to make server understand receiver) :
+// 100XXX
+
+// 101XXX
+const int FIRST_CONNECTION = 1010;
+const int AUTHENTICATE_CLIENT = 1011;
+
+// 102XXX
+
+//103XXX
