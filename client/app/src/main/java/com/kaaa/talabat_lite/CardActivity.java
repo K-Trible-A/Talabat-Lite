65388adaeefc0dcb42f275ecd8394d9e9e9dba08
AbdoAli003 <abdoali0042@gmail.com>
2024-11-24 17:53:27 +0200

    Modified card activity
    Modified card activity so that the card information does not remain after the program terminates

--- client/app/src/main/java/com/kaaa/talabat_lite/CardActivity.java
+++ client/app/src/main/java/com/kaaa/talabat_lite/CardActivity.java
@@ -1,9 +1,8 @@
-package com.kaaa.talabat_lite;
+package com.example.merchant;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,6 +30,7 @@ public class CardActivity extends AppCompatActivity {
         setupListeners();
 
     }
+
     protected void initUI() {
 
         cardNumber = findViewById(R.id.cardNumber);
@@ -39,50 +39,42 @@ public class CardActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.submitButton);
     }
 
-    protected void setupListeners ()
-    {
+    protected void setupListeners() {
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String cardNumberStr = cardNumber.getText().toString().trim();
-                String CVVStr = CVV.getText().toString().trim();
-                String expiryDateStr = expiryDate.getText().toString().trim();
-                if (cardNumberStr.length() < 16)
-                {
-                    runOnUiThread(() -> Toast.makeText(CardActivity.this, "card number has 16 numbers!", Toast.LENGTH_SHORT).show());
-                    return;
-                }
-                if (expiryDateStr.length() < 5)
-                {
-                    runOnUiThread(() -> Toast.makeText(CardActivity.this, "expiry date format is MM/YY", Toast.LENGTH_SHORT).show());
-                    return;
-                }
-                if (expiryDateStr.charAt(0) < '0' || expiryDateStr.charAt(0) > '9' || expiryDateStr.charAt(1) < '0' || expiryDateStr.charAt(1) > '9' || expiryDateStr.charAt(3) < '0' || expiryDateStr.charAt(3) > '9' || expiryDateStr.charAt(4) < '0' || expiryDateStr.charAt(4) > '9' || expiryDateStr.charAt(2) != '/')
-                {
-                    runOnUiThread(() -> Toast.makeText(CardActivity.this, "expiry date format is MM/YY", Toast.LENGTH_SHORT).show());
-                    return;
-                }
-                if ((expiryDateStr.charAt(0) >= '1' && expiryDateStr.charAt(1) >= '3') || (expiryDateStr.charAt(0) == '0' && expiryDateStr.charAt(1) == '0') || (expiryDateStr.charAt(3) == '0' && expiryDateStr.charAt(4) == '0') || (expiryDateStr.charAt(0) >= '2'))
-                {
-                    runOnUiThread(() -> Toast.makeText(CardActivity.this, "enter a valid date!", Toast.LENGTH_SHORT).show());
-                    return;
-                }
-                if (CVV.length() < 3)
-                {
-                    runOnUiThread(() -> Toast.makeText(CardActivity.this, "CVV has 3 numbers!", Toast.LENGTH_SHORT).show());
-                    return;
-                }
-                saveData(cardNumberStr, expiryDateStr, CVVStr);
-                outIntent = new Intent(CardActivity.this, MerchantRegistrationActivity.class);
-                outIntent.putExtra("cardNumber",cardNumberStr);
-                outIntent.putExtra("expiryDate",expiryDateStr);
-                outIntent.putExtra("CVV",CVVStr);
-                setResult(RESULT_OK,outIntent);
-                finish();
+        submitButton.setOnClickListener(v -> {
+            String cardNumberStr = cardNumber.getText().toString().trim();
+            String CVVStr = CVV.getText().toString().trim();
+            String expiryDateStr = expiryDate.getText().toString().trim();
+            if (cardNumberStr.length() < 16) {
+                runOnUiThread(() -> Toast.makeText(CardActivity.this, "card number has 16 numbers!", Toast.LENGTH_SHORT).show());
+                return;
             }
+            if (expiryDateStr.length() < 5) {
+                runOnUiThread(() -> Toast.makeText(CardActivity.this, "expiry date format is MM/YY", Toast.LENGTH_SHORT).show());
+                return;
+            }
+            if (expiryDateStr.charAt(0) < '0' || expiryDateStr.charAt(0) > '9' || expiryDateStr.charAt(1) < '0' || expiryDateStr.charAt(1) > '9' || expiryDateStr.charAt(3) < '0' || expiryDateStr.charAt(3) > '9' || expiryDateStr.charAt(4) < '0' || expiryDateStr.charAt(4) > '9' || expiryDateStr.charAt(2) != '/') {
+                runOnUiThread(() -> Toast.makeText(CardActivity.this, "expiry date format is MM/YY", Toast.LENGTH_SHORT).show());
+                return;
+            }
+            if ((expiryDateStr.charAt(0) >= '1' && expiryDateStr.charAt(1) >= '3') || (expiryDateStr.charAt(0) == '0' && expiryDateStr.charAt(1) == '0') || (expiryDateStr.charAt(3) == '0' && expiryDateStr.charAt(4) == '0') || (expiryDateStr.charAt(0) >= '2')) {
+                runOnUiThread(() -> Toast.makeText(CardActivity.this, "enter a valid date!", Toast.LENGTH_SHORT).show());
+                return;
+            }
+            if (CVV.length() < 3) {
+                runOnUiThread(() -> Toast.makeText(CardActivity.this, "CVV has 3 numbers!", Toast.LENGTH_SHORT).show());
+                return;
+            }
+            saveData(cardNumberStr, expiryDateStr, CVVStr);
+            outIntent = new Intent(CardActivity.this, MerchantRegistrationActivity.class);
+            outIntent.putExtra("cardNumber", cardNumberStr);
+            outIntent.putExtra("expiryDate", expiryDateStr);
+            outIntent.putExtra("CVV", CVVStr);
+            setResult(RESULT_OK, outIntent);
+            finish();
         });
     }
+
     private void saveData(String cardNumberStr, String expiryDateStr, String CVVStr) {
         SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -102,5 +94,23 @@ public class CardActivity extends AppCompatActivity {
         cardNumber.setText(savedCardNumber);
         expiryDate.setText(savedExpiryDate);
         CVV.setText(savedCVV);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Clear saved data when the activity is destroyed
+        clearSavedData();
+    }
+
+    private void clearSavedData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(KEY_CARD_NUMBER);
+        editor.remove(KEY_EXPIRY_DATE);
+        editor.remove(KEY_CVV);
+        editor.apply(); // Apply the changes asynchronously
+
     }
 }
