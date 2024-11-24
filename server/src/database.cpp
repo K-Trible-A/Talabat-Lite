ec6bf8bc0f8b94ce61e5ad9033d2c7604f1dc821
AbdoAli003 <abdoali0042@gmail.com>
2024-11-24 15:57:11 +0200

    modified merchant table schema
    Added cardId

--- server/src/database.cpp
+++ server/src/database.cpp
@@ -54,12 +54,12 @@ void Database::createSchema() {
       "password TEXT NOT NULL"
       ");");
   this->execute(  // Merchant information
-      "CREATE TABLE IF NOT EXISTS merchant (merchantId INTEGER PRIMARY KEY, businessName TEXT NOT NULL,"
+      "CREATE TABLE IF NOT EXISTS merchant (merchantId INTEGER PRIMARY KEY AUTOINCREMENT, cardId INTEGER NOT NULL ,businessName TEXT NOT NULL,"
       "businessType TEXT NOT NULL , keywords TEXT NOT NULL,"
       "pickupAddress TEXT NOT NULL , nationalID TEXT NOT NULL,"
-      "FOREIGN KEY(merchantId) REFERENCES card(cardId) , FOREIGN KEY(merchantId) REFERENCES users(id));");
+      "FOREIGN KEY(cardId) REFERENCES card(cardId) , FOREIGN KEY(merchantId) REFERENCES users(id));");
   this->execute( // Card information
-    "CREATE TABLE IF NOT EXISTS card (cardId INTEGER PRIMARY KEY ,"
+    "CREATE TABLE IF NOT EXISTS card (cardId INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "cardNumber TEXT NOT NULL , CVV TEXT NOT NULL,"
                 "expiryDate TEXT NOT NULL);");
 
