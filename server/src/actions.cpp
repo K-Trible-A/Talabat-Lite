2927e7167a941e89b81aa8853eb20ff17fb0f896
AbdoAli003 <abdoali0042@gmail.com>
2024-11-24 11:25:37 +0200

    Added a functions that inserts merchant's informations into database
    the functions recieves the information from the client and stores it in the db file

--- server/src/actions.cpp
+++ server/src/actions.cpp
@@ -1,6 +1,7 @@
 #include "../include/actions.hpp"
 #include "../include/database.hpp"
 #include "../include/server.hpp"
+#include <iostream>
 
 #include <string>
 using namespace std;
@@ -24,4 +25,38 @@ void authClient(int clientFD) {
   else
     ok = (pass == ans[0][0]);
   server::send(clientFD, ok);
+}
+
+void addMerchant(int clientFD)
+{
+  
+  enum {grocery = 1 , restaurant = 2 , pharmacy = 3};
+  string type;
+  string businessName = server::recvString(clientFD);
+  int businessType = server::recvInt(clientFD);
+  string keywords = server::recvString(clientFD);
+  string pickupAddress = server::recvString(clientFD);
+  string nationaID = server::recvString(clientFD);
+  string cardNumber = server::recvString(clientFD);
+  string expiryDate = server::recvString(clientFD);
+  string CVV = server::recvString(clientFD);
+  switch (businessType)
+  {
+      case 1:
+          type = "grocery";
+          break;
+      case 2:
+          type = "restaurant";
+          break;
+      case 3:
+          type = "pharmacy";
+          break; 
+  }
+  const string merchExec = "INSERT INTO merchant (businessName, businessType, keywords, pickupAddress, nationalID) VALUES ('" + businessName + "','" + type + "','" + keywords + "','" + pickupAddress + "','" + nationaID + "');";
+
+  const string cardExec = "INSERT INTO card (cardNumber,CVV,expiryDate) VALUES ('" + cardNumber + "','" + CVV + "','" + expiryDate + "');";
+      db.execute(merchExec);
+      db.execute(cardExec);
+      const int ok = 1;
+  server::send(clientFD,ok);
 }
