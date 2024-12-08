15f8a7ad0301b7fc5c4a3d6fcbf06159105a9fa1
AbdoAli003 <abdoali0042@gmail.com>
2024-12-08 11:49:57 +0200

    Modified merchant activity
    Fixed the problem that occurs after changing pickup address that the selected fragment at the bottom is home not profile.

--- client/app/src/main/java/com/kaaa/talabat_lite/MerchantActivity.java
+++ client/app/src/main/java/com/kaaa/talabat_lite/MerchantActivity.java
@@ -22,25 +22,34 @@ public class MerchantActivity extends AppCompatActivity {
         setContentView(R.layout.activity_merchant);
         initUI();
         setupListeners();
+
+        // Handle fragment from intent
         String fragmentName = getIntent().getStringExtra("fragment");
-        if (fragmentName != null)
-        {
-            switch (fragmentName)
-            {
+        if (fragmentName != null) {
+            switch (fragmentName) {
                 case "home":
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container,merchantHomeFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, merchantHomeFragment).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.home); // Update selected item
                     break;
                 case "orders":
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container,merchantOrdersFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, merchantOrdersFragment).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.orders); // Update selected item
                     break;
                 case "profile":
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container,merchantProfileFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, merchantProfileFragment).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.profile); // Update selected item
                     break;
                 case "search":
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container,merchantSearchFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, merchantSearchFragment).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.search); // Update selected item
+                    break;
             }
+        } else {
+            // Default behavior: load home fragment
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, merchantHomeFragment).commit();
         }
     }
+
     protected void initUI()
     {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
